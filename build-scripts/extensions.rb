b874924b22aa0245d585e77631c86bfee088e1f7
MoePad <moepad380@googlemail.com>
2017-12-24 11:54:22 +0100

    added examples for include and pre-processing

--- build-scripts/extensions.rb
+++ build-scripts/extensions.rb
@@ -27,3 +27,31 @@ Asciidoctor::Extensions.register do
     end
   end
 end
+
+Asciidoctor::Extensions.register do
+  preprocessor do
+    process do |document, reader|
+      Asciidoctor::Reader.new reader.readlines.map {|l|
+        if l.start_with? 'replace-during-preprocessing::[]'
+          next "this text was added during preprocessing"
+        end
+        next %(#{l})
+      }
+    end
+  end
+end
+
+# A processor, that will be called if any external resources are included (e.g. source code, URIs, other adoc files, ...)
+Asciidoctor::Extensions.register do
+  include_processor do
+    process do |doc, reader, target, attributes|
+      source = File.read File.join(target)
+      reader.push_include source.gsub("//include-processing::[]", "//INCLUDE PROCESSING"), target, target, 1, attributes
+      reader
+    end
+
+    def handles? target
+      target.end_with? '.java'
+    end
+  end
+end
