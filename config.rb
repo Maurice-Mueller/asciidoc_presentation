2592fc4e2fd094f5ef92d3ae2eae8c0a5d2e0f4a
MoePad <moepad380@googlemail.com>
2018-01-06 16:37:29 +0100

    improvement of title page

--- config.rb
+++ config.rb
@@ -1,6 +1,11 @@
+require './build-scripts/common'
+
 # PROJECT SETTINGS
-$project_name='project_name'
-$theme='beige'
+$project_name = 'Example Project'
+$author = 'Maurice Müller'
+$email = 'email@example.com'
+$project_version = 'v1.0'
+$theme = 'beige'
 
 # VERSIONS
 $revealjs_version = '3.6.0'
@@ -8,12 +13,13 @@ $decktape_version = '2.6.1'
 
 # DIRECTORIES
 $src_base = 'src'
+$resources_base = $src_base + '/resources'
 $doc_dir = $src_base + '/asciidoc'
 $index_file = $doc_dir + '/index.adoc'
-$video_dir = $src_base + '/resources/videos'
-$image_dir = $src_base + '/resources/images'
-$generated_images_dir = $src_base + '/resources/generated'
-$pdf_images_dir = $src_base + '/resources/pdf_images_dir'
+$video_dir = $resources_base + '/videos'
+$image_dir = $resources_base + '/images'
+$generated_images_dir = $resources_base + '/generated'
+$pdf_images_dir = $resources_base + '/pdf_images_dir'
 $out_dir = 'out'
 $revealjs_out_dir = $out_dir + '/revealjs'
 $revealjs_out_file = $revealjs_out_dir + '/' + $project_name + '.html'
@@ -23,13 +29,17 @@ $asciidoc_pdf_out_dir = $out_dir + '/pdf'
 $asciidoc_pdf_out_file = $asciidoc_pdf_out_dir + '/' + $project_name + '.pdf'
 $slides_pdf_out_dir = $out_dir + '/slides-pdf'
 $slides_pdf_file = $slides_pdf_out_dir + '/' + $project_name + '.pdf'
-$custom_config_dir = $src_base + '/resources/custom_config'
+$custom_config_dir = $resources_base + '/custom_config'
 
-$project_dir_escaped = "#{Dir.pwd}".gsub(/ /, '\ ')
+$project_dir_escaped = escapeString("#{Dir.pwd}")
 
 # SETTINGS
 $custom_attributes = ''
 $custom_attributes += " imagesoutdir=#{$project_dir_escaped}/#{$generated_images_dir}"
+$custom_attributes += " author=#{escapeString($author)}"
+$custom_attributes += " email=#{escapeString($email)}"
+$custom_attributes += " project_name=#{escapeString($project_name)}"
+$custom_attributes += " project_version=#{escapeString($project_version)}"
 
 # CODE
 # To add a custom language, use the following pattern:
